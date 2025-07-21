3c9a69baa0011826db5a22bc9587e384c3b760b1
taffy <ececanous@gmail.com>
2025-07-21 21:00:22 +1000

    first time push FS26-Notekeepers

--- frontend/FS26-notes-keeper/src/App.jsx
+++ frontend/FS26-notes-keeper/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import './App.css';
+import { useState } from 'react';
+import Header from './components/Header';
+import Note from './components/Note';
+import CreateNote from './components/CreateNote';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [notes, setNotes] = useState([{id: 1,title:'test title', content:'test content'}])
+const deleteNote =(itemToDeleteId)=>{
+  setNotes((prevNotes)=>{
+    return prevNotes.filter((noteItem )=>{
+      return itemToDeleteId !== noteItem.id
+    })
+  })
+}
+
+
+const addNote = (newNote)=>{
+  setNotes([...notes,newNote])
+
+}
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+    <Header/>
+
+    <CreateNote addNote = {addNote} />
+
+
+
+    {notes.map((noteItem, index)=>{
+
+      return (<Note key={`${noteItem.id}-${index}`} noteItem={noteItem}  deleteNote={deleteNote}
+      
+      />)
+    }
+
+    )}
+
+
     </>
+
   )
+
 }
 
 export default App
