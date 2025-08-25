a965c1d16c0326c0373c44b3271f017daf46bee1
taffy <ececanous@gmail.com>
2025-08-26 09:50:51 +1000

    123

--- frontend/mood-app/.eslintrc.js
+++ frontend/mood-app/.eslintrc.js
@@ -1,41 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-    jest: true,
+  env:{
+    browser:true,
+    es2021:true,
+    node:true,
+    jest:true,
   },
-
-
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
   ],
-
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["react"],
-
-  rules: {
-    "no-unused-vars": "error",//检测声明变量而未使用
-    "no-undef": "error", //使用未声明变量
-    "eqeqeq": "error",//强制使用全等===
-    "no-console": "warn",//内部写console会有警告但是不报错
-    "max-len": ["warn", { "code": 80 }],  //当行数超过80字符，报warning
-    "semi": ["error", "always"],  //
-    "react/react-in-jsx-scope": "off",
-
-
+  rules:{
+    "no-unused-vars":"error",//声明了变量但未使用
+    "no-undef":"error",//使用未声明的变量
+    "eqeqeq":"error",//强制使用全等===
+    "no-console":"warn",
+    "max-len":["warn",{"code":80}],
+    "semi":["error","always"],
+    "react/react-in-jsx-scope":"off",
   },
-
   settings: {
     react: {
       version: "detect",
     },
   },
-
-
-
-};
+}
